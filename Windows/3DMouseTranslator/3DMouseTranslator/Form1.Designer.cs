cd221152ddc362ca81d0f611e6ef63603c1de877
unknown <crazytiti@free.fr>
2014-05-12 00:39:44 +0200

    added button support to zoom / translate added model rotation on Z axe added controls to modify threshold value and factor

--- Windows/3DMouseTranslator/3DMouseTranslator/Form1.Designer.cs
+++ Windows/3DMouseTranslator/3DMouseTranslator/Form1.Designer.cs
@@ -33,13 +33,19 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_threshold = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_sensivity = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tb_sensivity)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 12);
+            this.button1.Location = new System.Drawing.Point(183, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(89, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Calibrate";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,7 +62,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "X Y Z";
             // 
@@ -66,11 +72,71 @@
             this.timer2.Interval = 1000;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Threshold";
+            // 
+            // cb_threshold
+            // 
+            this.cb_threshold.FormattingEnabled = true;
+            this.cb_threshold.Items.AddRange(new object[] {
+            "2",
+            "5",
+            "10",
+            "15",
+            "20",
+            "25"});
+            this.cb_threshold.Location = new System.Drawing.Point(72, 58);
+            this.cb_threshold.Name = "cb_threshold";
+            this.cb_threshold.Size = new System.Drawing.Size(53, 21);
+            this.cb_threshold.TabIndex = 5;
+            this.cb_threshold.Text = "5";
+            this.cb_threshold.SelectedIndexChanged += new System.EventHandler(this.cb_threshold_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(220, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Help : bouton 1 to zoom bouton 2 to translate";
+            // 
+            // tb_sensivity
+            // 
+            this.tb_sensivity.Location = new System.Drawing.Point(15, 98);
+            this.tb_sensivity.Maximum = 50;
+            this.tb_sensivity.Minimum = 5;
+            this.tb_sensivity.Name = "tb_sensivity";
+            this.tb_sensivity.Size = new System.Drawing.Size(257, 45);
+            this.tb_sensivity.TabIndex = 7;
+            this.tb_sensivity.Value = 12;
+            this.tb_sensivity.Scroll += new System.EventHandler(this.tb_sensivity_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sensivity";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 44);
+            this.ClientSize = new System.Drawing.Size(284, 179);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_sensivity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cb_threshold);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -78,6 +144,7 @@
             this.Name = "Form1";
             this.Text = "SWControl";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tb_sensivity)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +156,11 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_threshold;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar tb_sensivity;
+        private System.Windows.Forms.Label label4;
     }
 }
 
